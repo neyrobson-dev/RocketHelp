72f76549085fcbf067a86e4aa2bd82fb7fd7ad40
Ney Robson <ney.gama.external@k2partnering.com>
2022-07-21 23:24:23 -0300

    feat: adicionando integração com firebase e configurações

--- android/app/src/main/java/com/neyrobson/rockethelp/MainActivity.java
+++ android/app/src/main/java/com/neyrobson/rockethelp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rockethelp;
+package com.neyrobson.rockethelp;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/neyrobson/rockethelp/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/neyrobson/rockethelp/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.rockethelp.newarchitecture.modules;
+package com.neyrobson.rockethelp.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
